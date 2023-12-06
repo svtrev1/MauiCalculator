2c18aecc01f46a5f20dc0fce5f41754f72514cf5
svtrev <revtsova_st@edu.surgu.ru>
2023-12-07 01:13:27 +0500

    add functions AddToBD and AddToFile

--- ViewModel.cs
+++ ViewModel.cs
@@ -2,6 +2,10 @@
 using System.Windows.Input;
 using Microsoft.Maui.Graphics.Text;
 using System.Collections.ObjectModel;
+using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+
 
 
 namespace elme.ViewModel;
@@ -19,9 +23,15 @@ public class ViewModel : INotifyPropertyChanged
     public bool c1;
     private int sizeT;
     private int sizeF;
+    public string inputDB;
+    public string resultDB;
+    string connStr = "server=92.246.214.15;port=3306;user=ais_revcova1889_calculator;database=ais_revcova1889_calculator;password=RCHdGQHPOEzMz1KDirqWlTMV;";
+    string filePath = "/Users/svtrev/Desktop/file/fileTRY.txt";
+
 
     public ViewModel()
     {
+        
         c1 = false;
         check = true;
         InputText = "";
@@ -33,6 +43,43 @@ public class ViewModel : INotifyPropertyChanged
         FontSize = 80;
         sizeT = 13;
         sizeF = 60;
+        firstDB();
+        
+
+
+    }
+
+    public void firstDB()
+    { 
+        string sql1 = "SELECT result FROM Calculator ORDER BY id DESC LIMIT 4,1;";
+        string sql2 = "SELECT input FROM Calculator ORDER BY id DESC LIMIT 4,1;";
+        loadDB(sql1, sql2);
+        sql1 = "SELECT result FROM Calculator ORDER BY id DESC LIMIT 3,1;";
+        sql2 = "SELECT input FROM Calculator ORDER BY id DESC LIMIT 3,1;";
+        loadDB(sql1, sql2);
+        sql1 = "SELECT result FROM Calculator ORDER BY id DESC LIMIT 2,1;";
+        sql2 = "SELECT input FROM Calculator ORDER BY id DESC LIMIT 2,1;";
+        loadDB(sql1, sql2);
+        sql1 = "SELECT result FROM Calculator ORDER BY id DESC LIMIT 1,1;";
+        sql2 = "SELECT input FROM Calculator ORDER BY id DESC LIMIT 1,1;";
+        loadDB(sql1, sql2);
+        sql1 = "SELECT result FROM Calculator ORDER BY id DESC LIMIT 1;";
+        sql2 = "SELECT input FROM Calculator ORDER BY id DESC LIMIT 1;";
+        loadDB(sql1, sql2);
+
+    }
+
+    private void loadDB(string sql1, string sql2)
+    {
+        MySqlConnection conn = new MySqlConnection(connStr);
+        conn.Open();
+        MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+        object resultDB = cmd1.ExecuteScalar();
+        cmd1.ExecuteScalar();
+        MySqlCommand cmd2 = new MySqlCommand(sql2, conn);
+        object inputDB = cmd2.ExecuteScalar();
+        cmd2.ExecuteScalar();
+        AddToHistory("     " + inputDB + '=' + resultDB);
     }
 
     private void UpdateFontSize()
@@ -124,6 +171,13 @@ public class ViewModel : INotifyPropertyChanged
             {
                 c1 = true;
                 InputText = "Ошибка";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                conn.Open();
+                string sql = "INSERT INTO Calculator(isError, date, input, result) VALUES(1, CURDATE(), @input, 'Ошибка')";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@input", temp);
+                cmd.ExecuteScalar();
+                AddToFile(temp + '=' + InputText);
                 AddToHistory("     " + temp + '=' + InputText);
             }
             else
@@ -137,11 +191,26 @@ public class ViewModel : INotifyPropertyChanged
                     if (calculator.c)
                     {
                         InputText = "Ошибка";
+                        MySqlConnection conn = new MySqlConnection(connStr);
+                        conn.Open();
+                        string sql = "INSERT INTO Calculator(isError, date, input, result) VALUES(1, CURDATE(), @input, 'Ошибка')";
+                        MySqlCommand cmd = new MySqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("@input", calculator.s);
+                        cmd.ExecuteScalar();
+                        AddToFile(calculator.s + '=' + InputText);
                         AddToHistory("     " + calculator.s + '=' + InputText);
                     }
                     else
                     {
                         InputText = temp;
+                        MySqlConnection conn = new MySqlConnection(connStr);
+                        conn.Open();
+                        string sql = "INSERT INTO Calculator(isError, date, input, result) VALUES(0, CURDATE(), @input, @result)";
+                        MySqlCommand cmd = new MySqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("@input", calculator.s);
+                        cmd.Parameters.AddWithValue("@result", temp);
+                        cmd.ExecuteScalar();
+                        AddToFile(calculator.s + '=' + temp + "  ");
                         AddToHistory("     " + calculator.s + '=' + temp);
                     }
                 }
@@ -153,8 +222,27 @@ public class ViewModel : INotifyPropertyChanged
         }
     }
 
+    private void AddToFile(string calculation)
+    {
+        if (File.Exists(filePath))
+        {
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine(calculation);
+            }
+        }
+        else
+        {
+            using (StreamWriter writer = File.CreateText(filePath))
+            {
+                writer.WriteLine(calculation);
+            }
+        }
+    }
+
     private void AddToHistory(string calculation)
     {
+        
         CalculationHistory.Add(calculation);
 
         if (CalculationHistory.Count > 5)
